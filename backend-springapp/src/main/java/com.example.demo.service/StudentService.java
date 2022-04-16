129d4020a0fb820c5435731bddd7310ef85b5f38
Kunal150 <67556367+Kunal150@users.noreply.github.com>
2022-04-16 22:12:01 +0530

    Create StudentService.java

---
+++ backend-springapp/src/main/java/com.example.demo.service/StudentService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+ private StudentRepository studentRepository;
+	public Student registerStudent(Student student ) {
+	return studentRepository.save(student);	
+}
+	public List<Student> getStudents(){
+		return (List<Student>) studentRepository.findAll();
+	}
+		public void deleteStudent (@RequestParam Integer id) {
+			studentRepository.deleteById(id);
+		}
+		public Student updateStudent(Student student) {
+			Integer rollNumber = student.getRollNumber();
+			Student std = studentRepository.findById(rollNumber).get();
+			std.setName(student.getName());
+			std.setAddress(student.getAddress());
+			std.setPercentage(student.getPercentage());
+			return studentRepository.save(std);
+		}
+	
+	}
